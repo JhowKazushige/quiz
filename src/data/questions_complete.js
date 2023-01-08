a5463e6828e1e8e94eb134f5a4a2799b0f0fb9f8
lucaskazushige <kazushigethetan1@gmail.com>
2023-01-07 21:38:39 -0300

    quiz

---
+++ src/data/questions_complete.js
@@ -0,0 +1,176 @@
+const data = [
+  {
+    category: "Veterinário",
+    questions: [
+      {
+        question:
+          'O som "Tum Tum bate coração" da música de Elba Ramalho, descreve em parte a anatomia e fisiologia do funcionamento cardíaco. Esse som é auscultado durante as bulhas cardíacas que é referente a:',
+        options: [
+          "a) Abertura e fechamento das valvas atrioventriculares",
+          "b) Sístole e Diástole atrial",
+          "c) Sístole e Diástole ventricular",
+          "d) Contração atrial ou ventricular",
+        ],
+        answer: "a) Abertura e fechamento das valvas atrioventriculares",
+        //tip: "É uma tag de uma letra apenas",
+      },
+      {
+        question:
+          "O sangue venoso tem cor vermelho mais escuro que o sangue arterial. Essa cor púrpura mais escura do sangue venoso deve-se a:",
+        options: [
+          "a) Concentração diminuída de dióxido de Carbono",
+          "b) Concentração aumentada de oxigênio",
+          "c) Concentração aumentada de dióxido de Carbono",
+          "d) Equilíbrio ácido-base sanguíneo e linfa",
+        ],
+        answer: "c) Concentração aumentada de dióxido de Carbono",
+      },
+      {
+        question:
+          "Qual dos seguintes estados da respiração caracteriza-se por aumento da profundidade, frequência ou ambas e torna-se evidente após esforço físico?",
+        options: [
+          "a) Dispnéia",
+          "b) Hiperpneia",
+          "c) Eupneia",
+          "d) Normopneia",
+        ],
+        answer: "b) Hiperpneia",
+      },
+      {
+        question:
+          "Com relação ao sistema cardiovascular, qual das alternativas é verdadeira?",
+        options: [
+          "a) O sistema arterial é um sistema de alta resistência e alta complacência",
+          "b) O sistema venoso é um sistema de baixa resistência e baixa complacência",
+          "c) O sistema arterial é um sistema de baixa resistência e baixa complacência",
+          "d) O sistema venoso é um sistema de baixa resistência e alta complacência",
+        ],
+        answer:
+          "d) O sistema venoso é um sistema de baixa resistência e alta complacência",
+      },
+      {
+        question: "Quais as funções do Sistema cardiovascular?",
+        options: [
+          "a) É responsável por sustentar nosso corpo.",
+          "b) É responsável pela produção de energia",
+          "c) É responsável pela circulação do sangue, de modo a transportar os nutrientes e o oxigênio por todo o corpo",
+          "d) É responsável pelo armazenamento de sais minerais, principalmente de cálcio e de fósforo.",
+        ],
+        answer:
+          "c) É responsável pela circulação do sangue, de modo a transportar os nutrientes e o oxigênio por todo o corpo",
+      },
+      {
+        question: "A pressão sanguínea é mais baixa nos(as):",
+        options: ["a) artérias", "b) veias", "c) arteríolas", "d) capilares"],
+        answer: "b) veias",
+      },
+      {
+        question: "Quais são os músculos envolvidos na inspiração?",
+        options: [
+          "a) Diafragma",
+          "b) Masseter",
+          "c) Temporal pterigóideo medial ",
+          "d) Pubococcígeo",
+        ],
+        answer: "a) Diafragma",
+      },
+      {
+        question: "Qual a função do nó sinu-atrial?",
+        options: [
+          "a) É responsável pela geração de energia",
+          "b) É responsável pela geração de lipídios",
+          "c) É responsável pela contração mandibular",
+          "d) É responsável pela geração dos impulsos nervosos determinantes da contração cardíaca.",
+        ],
+        answer:
+          "d) É responsável pela geração dos impulsos nervosos determinantes da contração cardíaca.",
+      },
+      {
+        question:
+          "Que nome recebe a capa de gordura encontrada no axônio dos neurônios?",
+        options: [
+          "a) Nodo de ranvier",
+          "b) Bainha de mielina",
+          "c) Oligodendróglia",
+          "d) Micróglia",
+        ],
+        answer: "b) Bainha de mielina",
+      },
+      {
+        question:
+          "Que nome recebe a célula especializada na propagação do impulso nervoso?",
+        options: [
+          "a) Neutrófilos",
+          "b) Neurônios",
+          "c) Condrócitos",
+          "d) Miócitos",
+        ],
+        answer: "b) Neurônios",
+      },
+      {
+        question:
+          "Qual  o nome da  valva que faz a comunicação entre o  ventrículo  direto  e a  arteria  pulmonar?",
+        options: [
+          "a) valva tricuspide",
+          "b) válvula mitral",
+          "c) valva aortica",
+          "d) valva pulmonar",
+        ],
+        answer: "d) valva pulmonar",
+      },
+      {
+        question: "como funciona  os impulsos  do coração?",
+        options: [
+          "a) geralmente  em descompasso",
+          "b) sempre acelerado",
+          "c) com contração e relaxamento",
+          "d) com sistole  sem diastole",
+        ],
+        answer: "c) com contração e relaxamento",
+      },
+      {
+        question: "A sistole  e diastole,  tem como  finalidade::",
+        options: [
+          "a) bater",
+          "b) promover  bombeamento do sangue para o corpo.",
+          "c) promover  troca gasosa",
+          "d) regular  o sangue venoso",
+        ],
+        answer: "b) promover  bombeamento do sangue para o corpo.",
+      },
+      {
+        question: "A principal  função  do córtex  renal é :",
+        options: [
+          "a) filtrar o sangue",
+          "b) separar  os glóbulos  brancos",
+          "c) identificar  os glóbulos  vermelhos",
+          "d) Nenhuma  das alternativas",
+        ],
+        answer: "a) filtrar o sangue",
+      },
+      {
+        question: "No processo de formação da  urina, ocorre:",
+        options: [
+          "a) reabsorção",
+          "b) filtração",
+          "c) secreção",
+          "d) todas as alternativas",
+        ],
+        answer: "d) todas as alternativas",
+      },
+      {
+        question:
+          "Por onde se espera a atuação mais rápida de um medicamento no cérebro?",
+        options: [
+          "a) Injetado na veia",
+          "b) Por via oral",
+          "c) Inalado pelos pulmões",
+          "d) Injetado na artéria",
+        ],
+        answer: "c) Inalado pelos pulmões",
+      },
+    ],
+  },
+];
+
+export default data;
